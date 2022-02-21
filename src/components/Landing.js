8fc80620abdc833eef3cf8513bb6a899818ca5db
amin <aminqanbarii1992@gmail.com>
2022-02-21 02:38:41 -0800

    add styles to landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,6 +4,9 @@ import React, { useState, useEffect } from "react";
 import Loader from "./Loader";
 import Coin from "./Coin";
 
+//css
+import styles from "./Landing.module.css";
+
 //API
 import { getCoins } from "../services/api";
 
@@ -24,28 +27,33 @@ const Landing = () => {
     setSearch(event.target.value);
   };
 
-  const searchedCoins = coins.filter((item) => item.name.toLowerCase().includes(search.toLowerCase()))
+  const searchedCoins = coins.filter((item) =>
+    item.name.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <>
       <input
         type="text"
+        className={styles.input}
         placeholder="Search..."
         value={search}
         onChange={searchHandler}
       />
       {coins.length ? (
-        searchedCoins.map((item) => (
-          <Coin
-            key={item.id}
-            symbol={item.symbol}
-            name={item.name}
-            image={item.image}
-            price={item.current_price}
-            marketCap={item.market_cap}
-            changePrice={item.price_change_percentage_24h}
-          />
-        ))
+        <div className={styles.coinContainer}>
+          {searchedCoins.map((item) => (
+            <Coin
+              key={item.id}
+              symbol={item.symbol}
+              name={item.name}
+              image={item.image}
+              price={item.current_price}
+              marketCap={item.market_cap}
+              changePrice={item.price_change_percentage_24h}
+            />
+          ))}
+        </div>
       ) : (
         <Loader />
       )}
